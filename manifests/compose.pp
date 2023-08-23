859d9549218b105708749004c9bc7fdd33887c77
David Phayanouvong <david.phayanouvong@systemit.fr>
2023-08-23 09:24:03 +0200

    Update compose.pp

--- manifests/compose.pp
+++ manifests/compose.pp
@@ -29,6 +29,9 @@
 #   responsible for ensuring the URL points to the correct version and
 #   architecture.
 #
+# @param curl_ensure
+#   Whether or not the curl package is ensured by this module.
+#
 class docker::compose (
   Enum[present,absent] $ensure                 = present,
   Optional[String]               $version      = $docker::params::compose_version,
@@ -37,77 +40,107 @@ class docker::compose (
   Optional[Pattern['^((http[s]?)?:\/\/)?([^:^@]+:[^:^@]+@|)([\da-z\.-]+)\.([\da-z\.]{2,6})(:[\d])?([\/\w \.-]*)*\/?$']] $proxy = undef,
   Optional[String]               $base_url     = $docker::params::compose_base_url,
   Optional[String]               $raw_url      = undef,
+  Optional[Boolean]              $curl_ensure  = $docker::params::curl_ensure,
 ) inherits docker::params {
-  if $ensure == 'present' {
-    if $facts['os']['family'] == 'windows' {
-      
-    
-      $file_owner     = 'Administrator'
-      $docker_compose_location           = "${install_path}/${symlink_name}${file_extension}"
 
-      if $version == 'latest' {
-        $docker_compose_location_versioned = "${install_path}/docker-compose-$$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)${file_extension}"
-      } else {
-        $docker_compose_location_versioned = "${install_path}/docker-compose-${version}${file_extension}"
-      }
-    
+  if $facts['kernel'] == 'Linux' {
+    'Debian': {
+      ensure_packages('docker-compose-plugin', { ensure => pick($version,$ensure), require => defined(bool2str($docker::use_upstream_package_source)) ? { true => Apt::Source['docker'], false => undef } }) #lint:ignore:140chars
+    }
+    'RedHat': {
+      ensure_packages('docker-compose-plugin', { ensure => pick($version,$ensure), require => defined(bool2str($docker::use_upstream_package_source)) ? { true => Yumrepo['docker'], false => undef } }) #lint:ignore:140chars lint:ignore:unquoted_string_in_selector
+    }
+    default: {}
+  } else {
+    # mac & Windows OS
+    if $facts['os']['family'] == 'windows' {
+      $file_extension = '.exe'
+      $file_owner     = 'Administrator'
+    } else {
+      $file_extension = ''
+      $file_owner     = 'root'
+    }
+
+    $docker_compose_location           = "${install_path}/${symlink_name}${file_extension}"
+
+    if $version == 'latest' {
+      # TODO: should not work on Windows
+      #$docker_compose_location_versioned = "${install_path}/docker-compose-$$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)${file_extension}"
+    } else {
+      $docker_compose_location_versioned = "${install_path}/docker-compose-${version}${file_extension}"
+    }
+
+    if $ensure == 'present' {
       if $raw_url != undef {
         $docker_compose_url = $raw_url
       } else {
         if $version =~ /2[.]\d+[.]\d+$/ {
           $version_prepath = 'v'
         }
+
+        $docker_compose_url = "${base_url}/${version_prepath}${version}/docker-compose-${facts['kernel']}-${facts['os']['hardware']}${file_extension}"
       }
-    
-      $docker_compose_url = "${base_url}/${version_prepath}${version}/docker-compose-${facts['kernel']}-${facts['os']['hardware']}.exe"
-      
+
       if $proxy != undef {
         $proxy_opt = "--proxy ${proxy}"
       } else {
         $proxy_opt = ''
       }
-      $docker_download_command = "if (Invoke-WebRequest ${docker_compose_url} ${proxy_opt} -UseBasicParsing -OutFile \"${docker_compose_location_versioned}\") { exit 0 } else { exit 1}" # lint:ignore:140chars
 
-      $parameters = {
-        'proxy'                             => $proxy,
-        'docker_compose_url'                => $docker_compose_url,
-        'docker_compose_location_versioned' => $docker_compose_location_versioned,
-      }
+      if $facts['os']['family'] == 'windows' {
+        $docker_download_command = "if (Invoke-WebRequest ${docker_compose_url} ${proxy_opt} -UseBasicParsing -OutFile \"${docker_compose_location_versioned}\") { exit 0 } else { exit 1}" # lint:ignore:140chars
 
-      exec { "Install Docker Compose ${version}":
-        command  => epp('docker/windows/download_docker_compose.ps1.epp', $parameters),
-        provider => powershell,
-        creates  => $docker_compose_location_versioned,
+        $parameters = {
+          'proxy'                             => $proxy,
+          'docker_compose_url'                => $docker_compose_url,
+          'docker_compose_location_versioned' => $docker_compose_location_versioned,
+        }
+  
+        exec { "Install Docker Compose ${version}":
+          command  => epp('docker/windows/download_docker_compose.ps1.epp', $parameters),
+          provider => powershell,
+          creates  => $docker_compose_location_versioned,
+        }
+  
+        file { $docker_compose_location:
+          ensure  => 'link',
+          target  => $docker_compose_location_versioned,
+          require => Exec["Install Docker Compose ${version}"],
+        }
+      } else {
+        if $curl_ensure {
+          stdlib::ensure_packages(['curl'])
+        }
+  
+        exec { "Install Docker Compose ${version}":
+          path    => '/usr/bin/',
+          cwd     => '/tmp',
+          command => "curl -s -S -L ${proxy_opt} ${docker_compose_url} -o ${docker_compose_location_versioned}",
+          creates => $docker_compose_location_versioned,
+          require => Package['curl'],
+        }
+  
+        file { $docker_compose_location_versioned:
+          owner   => $file_owner,
+          mode    => '0755',
+          seltype => 'container_runtime_exec_t',
+          require => Exec["Install Docker Compose ${version}"],
+        }
+  
+        file { $docker_compose_location:
+          ensure  => 'link',
+          target  => $docker_compose_location_versioned,
+          require => File[$docker_compose_location_versioned],
+        }
       }
-
-      file { $docker_compose_location:
-        ensure  => 'link',
-        target  => $docker_compose_location_versioned,
-        require => Exec["Install Docker Compose ${version}"],
-      }
+      
     } else {
-      case $facts['os']['family'] {
-        'Debian': {
-          ensure_packages('docker-compose-plugin', { ensure => pick($version,$ensure), require => defined(bool2str($docker::use_upstream_package_source)) ? { true => Apt::Source['docker'], false => undef } }) #lint:ignore:140chars
-        }
-        'RedHat': {
-          ensure_packages('docker-compose-plugin', { ensure => pick($version,$ensure), require => defined(bool2str($docker::use_upstream_package_source)) ? { true => Yumrepo['docker'], false => undef } }) #lint:ignore:140chars lint:ignore:unquoted_string_in_selector
-        }
-        default: {}
-      }
-    }
-  } else {
-    if $facts['os']['family'] == 'windows' {
       file { $docker_compose_location_versioned:
         ensure => absent,
       }
-
+  
       file { $docker_compose_location:
         ensure => absent,
-      }
-    } else {
-      package { 'docker-compose-plugin':
-        ensure => absent
       }
     }
   }
