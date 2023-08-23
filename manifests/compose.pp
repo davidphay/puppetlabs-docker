705a13b088c12d99817a9cb2153ead12fdb9ed93
David Phayanouvong <david.phayanouvong@systemit.fr>
2023-08-23 13:31:07 +0200

    Update compose.pp

--- manifests/compose.pp
+++ manifests/compose.pp
@@ -12,24 +12,26 @@ class docker::compose (
   Optional[String]     $version = $docker::params::compose_version,
 ) inherits docker::params {
 
-  if $version and $ensure != 'absent' {
-    $package_ensure = $version
-  } else {
-    $package_ensure = $ensure
-  }
-
-  case $facts['os']['family'] {
-    'Debian': {
-      ensure_packages('docker-compose-plugin', { ensure => $package_ensure, require => defined(bool2str($docker::use_upstream_package_source)) ? { true => Apt::Source['docker'], false => undef } }) #lint:ignore:140chars
+  if $docker::manage_package {
+    if $version and $ensure != 'absent' {
+      $package_ensure = $version
+    } else {
+      $package_ensure = $ensure
     }
-    'RedHat': {
-      ensure_packages('docker-compose-plugin', { ensure => $package_ensure, require => defined(bool2str($docker::use_upstream_package_source)) ? { true => Yumrepo['docker'], false => undef } }) #lint:ignore:140chars lint:ignore:unquoted_string_in_selector
-    }
-    'Windows': {
-      fail('Docker compose is installed with docker machine on Windows')
-    }
-    default: {
-      fail('This module only works on Debian, RedHat or Windows.')
+  
+    case $facts['os']['family'] {
+      'Debian': {
+        ensure_packages('docker-compose-plugin', { ensure => $package_ensure, require => defined(bool2str($docker::use_upstream_package_source)) ? { true => Apt::Source['docker'], false => undef } }) #lint:ignore:140chars
+      }
+      'RedHat': {
+        ensure_packages('docker-compose-plugin', { ensure => $package_ensure, require => defined(bool2str($docker::use_upstream_package_source)) ? { true => Yumrepo['docker'], false => undef } }) #lint:ignore:140chars lint:ignore:unquoted_string_in_selector
+      }
+      'Windows': {
+        fail('Docker compose is installed with docker machine on Windows')
+      }
+      default: {
+        fail('This module only works on Debian, RedHat or Windows.')
+      }
     }
   }
 }
